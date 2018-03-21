7aca8cb8283dce5ec7227fd1925be99c11c5a420
Emma M <mdopure@gmail.com>
2018-03-21 11:10:01 +0700

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // db.collection('Todos').find({ _id: new ObjectID('5ab110d866f111b61b2e44ba') }).toArray()
+  // .then((docs) => {
+  //   console.log('TODOS');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // })
+  // .catch((err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  // db.collection('Todos').find().count()
+  // .then((count) => {
+  //   console.log(`TODOS count: ${count}`);
+  // })
+  // .catch((err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  db.collection('Users').find({ name: 'Emma' }).count()
+  .then((count) => {
+    console.log(`${count} user${count === 1 ? '' : 's'} with that name.`);
+  })
+  .catch((err) => {
+    console.log('Unable to fetch todos', err)
+  });
+
+  // db.close();
+})
