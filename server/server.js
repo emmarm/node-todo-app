4cee3b09d937eafcf1900219822dac6ca2427d62
Emma M <mdopure@gmail.com>
2018-03-22 21:23:08 +0700

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -20,12 +21,22 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find()
-    .then((todos) => {
-      res.send({ todos });
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+    .then((todos) => res.send({ todos }))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    }).catch((err) => res.status(400).send());
 });
 
 app.listen(3000, () => console.log('Server running on port 3000.'));
