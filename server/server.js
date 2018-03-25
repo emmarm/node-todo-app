5cdb540fca6b6eafde7e9eeb0beeead7682f79c9
Emma M <mdopure@gmail.com>
2018-03-25 15:53:41 +0700

    Added POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT;
@@ -20,13 +21,13 @@ app.post('/todos', (req, res) => {
 
   todo.save()
     .then((doc) => res.send(doc))
-    .catch((err) => res.status(400).send(err));
+    .catch((e) => res.status(400).send(e));
 });
 
 app.get('/todos', (req, res) => {
   Todo.find()
     .then((todos) => res.send({ todos }))
-    .catch((err) => res.status(400).send(err));
+    .catch((e) => res.status(400).send(e));
 });
 
 app.get('/todos/:id', (req, res) => {
@@ -40,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
       }
       res.send({ todo });
-    }).catch((err) => res.status(400).send());
+    }).catch((e) => res.status(400).send());
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -79,6 +80,16 @@ app.patch('/todos/:id', (req, res) => {
 
     res.send({ todo });
   }).catch((e) => res.status(400).send());
+});
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user.save()
+    .then(() => user.generateAuthToken())
+    .then((token) => res.header('x-auth', token).send(user))
+    .catch((e) => res.status(400).send(e));
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
