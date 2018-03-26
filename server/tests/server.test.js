ec20cfab1e52ca9d94cfca8b79d179773f26ef9e
Emma M <mdopure@gmail.com>
2018-03-26 11:15:42 +0700

    Added POST /users/login tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -220,7 +220,7 @@ describe('POST /users', () => {
             expect(user).toExist();
             expect(user.password).toNotBe(password);
             done();
-          });
+          }).catch((e) => done(e));
       });
   });
 
@@ -243,5 +243,53 @@ describe('POST /users', () => {
       .send({ email, password })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should log in user and return auth token', (done) => {
+    const { email, password } = dummyUsers[1];
+
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+
+        User.findById(dummyUsers[1]._id)
+          .then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 401 for invalid user login', (done) => {
+    const { email } = dummyUsers[1];
+    const password = 'invalidPassword';
+
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+
+        User.findById(dummyUsers[1]._id)
+          .then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => done(e));
+      });
   });
 });
